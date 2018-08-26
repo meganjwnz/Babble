b7d61303584d417c312eff92051df6a23d692673
Megan Brown <mjones29@my.westga.edu>
2018-08-26 00:16:18 -0400

    Created TileManager class to create cellFactory. Changed playableTiles to use implement cell factory rather than add characters.

--- src/main/java/edu/westga/cs/babble/controllers/BabbleController.java
+++ src/main/java/edu/westga/cs/babble/controllers/BabbleController.java
@@ -1,24 +1,24 @@
 package edu.westga.cs.babble.controllers;
 
-import java.awt.event.MouseEvent;
-import java.beans.EventHandler;
-
+import javafx.util.Callback;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import edu.westga.cs.babble.model.EmptyTileBagException;
 import edu.westga.cs.babble.model.PlayedWord;
 import edu.westga.cs.babble.model.Tile;
 import edu.westga.cs.babble.model.TileBag;
 import edu.westga.cs.babble.model.TileNotInGroupException;
 import edu.westga.cs.babble.model.TileRack;
-
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.util.converter.NumberStringConverter;
-
-import javafx.event.ActionEvent;
 import javafx.fxml.*;
 import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
+
+import javafx.util.StringConverter;
+import javafx.scene.control.cell.TextFieldListCell;
+import javafx.util.Callback;
 
 public class BabbleController {
 
@@ -56,20 +56,13 @@ public class BabbleController {
 		this.playableTiles();
 		this.playerScoreResult();
 	}
-	
-	//Binds score to Integer
+
+	// Binds score to Integer
 	public void playerScoreResult() {
 		this.playerScore.textProperty().bindBidirectional(this.forceInteger, new NumberStringConverter());
 	}
-	
-	//Adds tiles to the List View
-	private void playableTiles() {
-		for (int tile = 0; tile < this.tileRack.MAX_SIZE; tile++) {
-			this.randomTiles.getItems().add(this.tileRack.tiles().get(tile).getLetter());
-		}
-	}
 
-	//Adds the maximum number of tiles to the tileRack
+	// Adds the maximum number of tiles to the tileRack
 	private void createRandomTiles() {
 		for (int numberOfTilesInRack = 0; numberOfTilesInRack < this.tileRack.MAX_SIZE; numberOfTilesInRack++) {
 			if (this.tileBag.isEmpty()) {
@@ -83,5 +76,52 @@ public class BabbleController {
 			}
 			this.tileRack.append(tile);
 		}
+	}
+
+	private class TileManager implements Callback<ListView<Tile>, ListCell<Tile>> {
+		public ListCell<Tile> call(ListView<Tile> listView) {
+			TextFieldListCell<Tile> cellFactory = new TextFieldListCell();
+			cellFactory.setConverter(new StringConverter() {
+				
+				@Override
+				public String toString(Object tile) {
+					String letter = ((Tile) tile).getLetter() + "";
+					return letter;
+				}
+
+				@Override
+				public Object fromString(String string) {
+					return null;
+				}
+				
+			});
+			return cellFactory;
+		}
+	}
+
+	// Adds tiles to the List View
+	private void playableTiles() {
+		this.randomTiles.setItems(this.tileRack.tiles());
+		this.randomTiles.setCellFactory(new TileManager());
+		this.randomTiles.setOnMouseClicked(new EventHandler() {
+
+			@Override
+			public void handle(Event event) {
+				Tile selection = (Tile) BabbleController.this.randomTiles.getSelectionModel().getSelectedItem();
+				try {
+					BabbleController.this.tileRack.remove(selection);
+					BabbleController.this.word.append(selection);
+				} catch (TileNotInGroupException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+
+	}
+
+	// Adds tiles to play area
+	public void displaySelection(MouseEvent event) {
+		this.playedTiles.setItems(this.word.tiles());
+		Tile letter = (Tile) this.randomTiles.getSelectionModel().getSelectedItem();
 	}
 }
