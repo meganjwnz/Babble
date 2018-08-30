998c554813f90d9cb5c0710a0a6d7d50a64f9237
Megan Brown <mjones29@my.westga.edu>
2018-08-29 22:05:10 -0400

    JUnit testing of TileRack#getNumberOfTilesNeeded

--- src/test/java/edu/westga/cs/babble/model/TestTileRackGetNumberOfTilesNeeded.java
+++ src/test/java/edu/westga/cs/babble/model/TestTileRackGetNumberOfTilesNeeded.java
@@ -1,5 +1,52 @@
 package edu.westga.cs.babble.model;
 
-public class TestTileRackGetNumberOfTilesNeeded {
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Test;
+
+/**
+ * tests for the TileRack#getNumberOfTilesNeeded() method
+ * @author Megan Brown
+ *
+ */
+public class TestTileRackGetNumberOfTilesNeeded {
+	
+	@Test
+	public void emptyTileRackShouldNeedMaxSizeNumberOfTiles() {
+		TileRack tileRack = new TileRack();
+		assertEquals(7, tileRack.getNumberOfTilesNeeded());		
+	}
+	
+	@Test
+	public void tileRackWithOneTileShouldNeedMaxSizeMinusOneTiles() {
+		TileRack tileRack = new TileRack();
+		tileRack.append(new Tile('E'));
+		assertEquals(6, tileRack.getNumberOfTilesNeeded());
+		assertEquals("E", tileRack.getHand());
+	}
+	
+	@Test
+	public void tileRackWithSeveralTilesShouldNeedSomeTiles() {
+		TileRack tileRack = new TileRack();
+		tileRack.append(new Tile('E'));
+		tileRack.append(new Tile('A'));
+		tileRack.append(new Tile('Q'));
+		tileRack.append(new Tile('R'));
+		assertEquals(3, tileRack.getNumberOfTilesNeeded());
+		assertEquals("EAQR", tileRack.getHand());
+	}
+	
+	@Test
+	public void fullRackNeedsZeroTiles() {
+		TileRack tileRack = new TileRack();
+		tileRack.append(new Tile('E'));
+		tileRack.append(new Tile('A'));
+		tileRack.append(new Tile('Q'));
+		tileRack.append(new Tile('R'));
+		tileRack.append(new Tile('P'));
+		tileRack.append(new Tile('L'));
+		tileRack.append(new Tile('M'));
+		assertEquals(0, tileRack.getNumberOfTilesNeeded());
+		assertEquals("EAQRPLM", tileRack.getHand());
+	}
 }
