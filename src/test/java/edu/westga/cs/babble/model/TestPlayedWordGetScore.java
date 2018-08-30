84b8644a0200b3e50527e47c173b348e85ed308c
Megan Brown <mjones29@my.westga.edu>
2018-08-29 22:18:30 -0400

    JUnit tested PlayWord#getScore

--- src/test/java/edu/westga/cs/babble/model/TestPlayedWordGetScore.java
+++ src/test/java/edu/westga/cs/babble/model/TestPlayedWordGetScore.java
@@ -1,5 +1,46 @@
 package edu.westga.cs.babble.model;
 
-public class TestPlayedWordGetScore {
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Test;
+
+/**
+ * tests for the PlayedWord#getScore() method
+ * @author Megan Brown
+ *
+ */
+public class TestPlayedWordGetScore {
+	
+	@Test
+	public void emptyWordShouldHaveScoreOfZero() {
+		PlayedWord playedWord = new PlayedWord();
+		assertEquals(0, playedWord.getScore());
+	}
+	
+	@Test
+	public void scoreAOneTileWord() {
+		PlayedWord playedWord = new PlayedWord();
+		playedWord.append(new Tile('I'));
+		assertEquals(1, playedWord.getScore());
+	}
+	
+	@Test
+	public void scoreAWordWithMultipleDifferingTiles() {
+		PlayedWord playedWord = new PlayedWord();
+		playedWord.append(new Tile('P'));
+		playedWord.append(new Tile('I'));
+		playedWord.append(new Tile('G'));
+		assertEquals(6, playedWord.getScore());
+	}
+	
+	@Test
+	public void scoreAWordContainingDuplicateTiles() {
+		PlayedWord playedWord = new PlayedWord();
+		playedWord.append(new Tile('P'));
+		playedWord.append(new Tile('I'));
+		playedWord.append(new Tile('G'));
+		playedWord.append(new Tile('G'));
+		playedWord.append(new Tile('Y'));
+		assertEquals(12, playedWord.getScore());
+	}
 }
